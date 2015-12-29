277e3005fd49160f08f1d54c3e3bfddd797092a9
John Carter <john@therefromhere.org>
2015-12-29 14:34:43 +1300

    work in progress on rules + board iteration

--- puzzle.py
+++ puzzle.py
@@ -30,7 +30,75 @@ def init_board(size):
     return board
 
 
+def transpose_board(board):
+    transposed_board = [[ board[x][y] for x in range(len(board[y]))  ] for y in range(len(board))]
+
+    return transposed_board
+
+
+def init_rules():
+    row_rules = (
+                    (7, 3, 1, 1, 7, ),
+                 (1, 1, 2, 2, 1, 1, ),
+           (1, 3, 1, 3, 1, 1, 3, 1, ),
+           (1, 3, 1, 1, 6, 1, 3, 1, ),
+           (1, 3, 1, 5, 2, 1, 3, 1, ),
+                    (1, 1, 2, 1, 1, ),
+              (7, 1, 1, 1, 1, 1, 7, ),
+                             (3, 3, ),
+        (1, 2, 3, 1, 1, 3, 1, 1, 2, ),
+                 (1, 1, 3, 2, 1, 1, ),
+                 (4, 1, 4, 2, 1, 2, ),
+        # ...
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+    )
+
+    col_rules = (
+    )
+
+    return row_rules, col_rules
+
+
+def iterate_row(row, rules):
+    return row
+
+
+def iterate_board(board, row_rules, col_rules):
+
+    for i, row in enumerate(board):
+        #print(i, row)
+        #print(row_rules[i])
+        board[i] = iterate_row(row, row_rules[i])
+
+    return board
+
+
 if __name__ == "__main__":
     board = init_board(BOARD_SIZE)
+    row_rules, col_rules = init_rules()
+
+    #print_board(board)
+    #print('------------')
+    #board = transpose_board(board)
+    #print_board(board)
+    #print('------------')
+    #board = transpose_board(board)
+    #print_board(board)
+
+
+    board = iterate_board(board, row_rules, col_rules)
 
     print_board(board)
