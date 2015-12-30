d710d10c5b62f5994bd7eeba803beff8f13b0010
John Carter <john@therefromhere.org>
2015-12-31 11:40:44 +1300

    Added border to be more QR-code reader-friendly

--- print.py
+++ print.py
@@ -22,7 +22,10 @@ def print_board(board, row_marker=None, col_marker=None):
     if col_marker is None:
         print('')
     else:
-        print(col_marker * 2 * ' ', '*')
+        print((col_marker * 2  + 2) * ' ', '*')
+
+    border = ' ' + ''.join((len(board) + 2) * display[CELL_EMPTY])
+    print(border)
 
     for i, row in enumerate(board):
         if row_marker == i:
@@ -31,4 +34,7 @@ def print_board(board, row_marker=None, col_marker=None):
             print(' ', end='')
 
         row_letters = [display[c] for c in row]
-        print(*row_letters, sep='')
+        row_display = [display[CELL_EMPTY]] + row_letters + [display[CELL_EMPTY]]
+        print(*row_display, sep='')
+
+    print(border)
